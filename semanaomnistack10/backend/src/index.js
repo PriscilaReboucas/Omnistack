9a89d26d39158c1affde56ddf4d4aa318a07c15a
Priscila <priscilaresantos@gmail.com>
2020-01-15 02:01:39 -0300

    teste

--- semanaomnistack10/backend/src/index.js
+++ semanaomnistack10/backend/src/index.js
@@ -7,8 +7,8 @@ const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-edjru.mongodb.net/week10?retryWrites=true&w=majority',{
     useNewUrlParser: true,
-    useUnifiedTopology: true, 
-});
+    useUnifiedTopology: true,
+    });
 
 // le as requisições json
 app.use(express.json()); 
